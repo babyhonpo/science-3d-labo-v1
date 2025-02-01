b527fba96ac352ddf65d21298ce827f9cb9f5f1a
taku10101 <taku1011ht@gmail.com>
2025-02-02 05:37:05 +0900

    add bom

--- src/utils/collisionRules.ts
+++ src/utils/collisionRules.ts
@@ -1,22 +1,22 @@
 // **衝突ルールをオブジェクトのペアとして管理**
 export const collisionRules = new Map<
-    string, // "box-sphere" のような文字列のキー
-    string
+  string, // "box-sphere" のような文字列のキー
+  string
 >([
-    ["Tc-Rh", "explosioneffect"],
-    ["Rh-Tc", "ac"],
-    ["B-C", "Tc"],
-    ["C-B", "Tc"],
+  ["Tc-Rh", "Bom"],
+  ["Rh-Tc", "Bom"],
+  ["B-C", "Bom"],
+  ["C-B", "Bom"],
 ]);
 
 // **衝突結果を取得**
 export const getCollisionResult = (
-    typeA: string,
-    typeB: string
+  typeA: string,
+  typeB: string
 ): string | null => {
-    // **キーを統一するためにソート**
-    const key = [typeA, typeB].sort().join("-"); // "box-sphere"
-    console.log(typeA, typeB);
-    console.log("衝突しました");
-    return collisionRules.get(key) || null;
+  // **キーを統一するためにソート**
+  const key = [typeA, typeB].sort().join("-"); // "box-sphere"
+  console.log(typeA, typeB);
+  console.log("衝突しました");
+  return collisionRules.get(key) || null;
 };
