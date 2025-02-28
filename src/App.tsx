0ce60f468c6b08d99518336adf700fe1a5073242
taku10101 <taku1011ht@gmail.com>
2025-02-28 21:21:12 +0900

    テストページデリート

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import Home from "./route/Home.tsx";
 import Top from "./route/Top";
-import Effect from "./route/Effect";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
@@ -20,9 +19,8 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path="/" element={<Top />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/test/effect" element={<Effect />} />
+        <Route path='/' element={<Top />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </ThemeProvider>
   );
