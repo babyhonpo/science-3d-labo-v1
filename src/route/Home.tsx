3284148b2a0e69b7a084dc062440492499671c07
yamada_co_jp <f.atsuya.personal@gmail.com>
2025-04-18 00:03:42 +0900

    周期表を開くボタンのUIを変更

--- src/route/Home.tsx
+++ src/route/Home.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import {PeriodicTable} from "../components/the_periodic_table/periodic-table";
-import { Box, Modal, Button } from "@mui/material";
+import { Box, Modal } from "@mui/material";
 import { useSceneLogic } from "../hooks/useSceneLogic";
 import { SceneCanvas } from "../components/SceneCanvas";
-// import PeriodicTable from "../components/PeriodicTable";
 import { ObjectType } from "../types/types";
+import Orb  from "../components/Orb";
 
 export default function Home(){
   const {
@@ -23,6 +23,17 @@ export default function Home(){
     ((type: ObjectType ) => void) | null
   >(null);
 
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const handleCloseModal =() => {
+    handleClose();
+    setTimeout(() => {
+      if (buttonRef.current) {
+        buttonRef.current.blur(); // ボタンのフォーカスを外す
+      }
+    }, 0);
+  }
+
   return (
     // 画面いっぱいにCanvasが表示されるようdivでラップしている
     <div style={{ width: "100vw", height: "100vh" }}>
@@ -36,28 +47,59 @@ export default function Home(){
         onAddItemToFront={( fn ) => setAddItemFront(() => fn)}
       />
       <Box
+        ref={buttonRef}
+        // variant="contained"
+        onClick={handleOpen}
         sx={{
-          display: "flex",
+          display: "inline-flex",
           justifyContent: "center",
-          width: "100%",
+          width: "fit-content",
+          height: "fit-content",
           position: "absolute",
-          top: "90%",
+          bottom: "20px",
+          left: "50%",
+          transform: "translate(-50%)",
+          cursor: "pointer",
         }}
       >
-        <Button
-          variant="contained"
-          onClick={handleOpen}
-          sx={{
-            fontSize: "1.4rem",
-          }}
-        >
-          周期表を開く
-        </Button>
+        <div
+        style={{
+          width: '100%',
+          height: '180px',
+          position: 'relative',
+        }}>
+            <Orb
+              hoverIntensity={0.5}
+              rotateOnHover={true}
+              hue={0}
+              forceHoverState={false}
+            />
+
+            <div
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                color: "white",
+                fontSize: "1.2rem",
+                fontWeight: "bold",
+                pointerEvents: "none",
+              }}
+            >
+              <p
+                style={{
+                  color: "#718FCD",
+                }}>
+                元素を召喚
+              </p>
+            </div>
+        </div>
       </Box>
 
       <Modal
         open={isModalOpen}
-        onClose={handleClose}
+        onClose={handleCloseModal}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
